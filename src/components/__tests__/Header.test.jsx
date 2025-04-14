4c5ca1282a0f79eec5a5419e1264c23dba5305cf
StevenScript <sdn709466@gmail.com>
2025-04-14 01:59:58 -0230

    FIX - Typo in header import

--- src/components/__tests__/Header.test.jsx
+++ src/components/__tests__/Header.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../header";
+import Header from "../Header";
 
 // Import jest-dom to extend the expect matchers
 import "@testing-library/jest-dom";

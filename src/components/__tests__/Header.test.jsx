d3dd46384d025ac6ee36d8dd0a16a32d39840b67
StevenScript <sdn709466@gmail.com>
2025-04-14 17:21:55 -0230

    FIX - cleaned up typos and unnecessary files

--- src/components/__tests__/Header.test.jsx
+++ src/components/__tests__/Header.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../Header";
+import Header from "../header";
 
 // Import jest-dom to extend the expect matchers
 import "@testing-library/jest-dom";
